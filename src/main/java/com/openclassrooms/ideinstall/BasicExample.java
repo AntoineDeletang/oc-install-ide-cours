693b936ab71793bfad73fbd18010d2d4ac735c29
AntoineDeletang <antdeletang@hotmail.fr>
2024-01-02 12:35:04 -0500

    Dernier test

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -5,7 +5,7 @@ public class BasicExample {
 	public static void main(String[] args) {
 		int index = 10;
 		for(int i=0; i<index; i++) {
-			System.out.println("lalalla");
+			System.out.println("ratio btw");
 		}
 	}
 }
